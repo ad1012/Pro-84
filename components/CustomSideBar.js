6228433bad160698a317f258f44a475d209ce090
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-05-30 22:53:03 -0500

    Add files via upload

--- components/CustomSideBar.js
+++ components/CustomSideBar.js
@@ -23,26 +23,3 @@ export default class CustomSideBar extends React.Component{
           } 
         }
     
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-      },
-      drawerItemsContainer: {
-        flex: 0.8,
-      },
-      logOutContainer: {
-        flex: 0.2,
-        justifyContent: "flex-end",
-        paddingBottom: 30,
-      },
-      logOutButton: {
-        height: 60,
-        width: "100%",
-        justifyContent: "center",
-        padding: 10,
-      },
-      logOutText: {
-        fontSize: 30,
-        fontWeight: "bold",
-      },
-    });
