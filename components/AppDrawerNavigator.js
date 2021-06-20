9df53c1d66e3d034e97cea14f9ddc4e2f51e3d4a
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-06-19 23:15:23 -0500

    Add files via upload

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { createDrawerNavigator } from "react-navigation-drawer";
+import MyBarters from "../screens/MyBarters";
 import SettingsScreen from "../screens/SettingsScreen";
 import {AppTabNavigator} from "./AppTabNavigator";
 import CustomSideBar from "./components/CustomSideBar";
 
 export const AppDrawerNavigator = createDrawerNavigator(
   {
-    Home: { 
-        screen: AppTabNavigator 
+  Home: { 
+      screen: AppTabNavigator 
     },
-   
+  MyBarters:{
+    screen: MyBarters
+  },
   Settings: {
     screen: SettingsScreen
   },
