cceaa833baaade81f5ed32e705ab1ae9b0cfacd0
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-06-26 16:23:59 -0500

    Add files via upload

--- components/AppTabNavigator.js
+++ components/AppTabNavigator.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {createBottomTabNavigator} from 'react-navigation-tabs'
 import ExchangeScreen from '../screens/ExchangeScreen';
-import HomeScreen from '../screens/HomeScreen';
-import HomeScreen from './screens/HomeScreen';
+import {AppStackNavigator} from './AppStackNavigator'
 import ExchangeScreen from './screens/ExchangeScreen';
 
-export const AppTabNavigator=createBottomTabNavigator({
+export const AppTabNavigator = createBottomTabNavigator({
     HomeScreen:{
-        screen:HomeScreen,
+        screen: AppStackNavigator,
         navigationOptions:{
             tabBarIcon:<Image source = {require("./assets/Home.png")}
             styles = {{width:20,height:20}}/>,
